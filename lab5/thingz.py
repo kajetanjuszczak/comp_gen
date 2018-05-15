6d7dddd33bd7af5fa0db21b15f83b1c17a29ca2c
xnestea <juszczak.kajetan@gmail.com>
2018-05-15 15:17:29 +0200

    5lab start

---
+++ lab5/thingz.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 15 14:59:38 2018
+
+@author: u2355
+"""
+
+perl ./inparanoid.pl 03.fa.txt.pfa 20.fa.txt.pfa 
+perl ./inparanoid.pl 03.fa.txt.pfa 51.fa.txt.pfa 
